f5ad79bf0f75ddcfbc460e517ea08c862b20b1a2
pp <paperplane025@gmail.com>
2026-05-28 20:03:47 +0700

    Update config

--- src/configs/env.ts
+++ src/configs/env.ts
@@ -3,7 +3,5 @@ export const env = {
   appName: import.meta.env.VITE_APP_NAME ?? 'CMB Recruitment',
   isDev: import.meta.env.DEV,
   enableMockApi:
-    import.meta.env.VITE_ENABLE_MOCK_API === 'true' ||
-    (import.meta.env.DEV &&
-      import.meta.env.VITE_ENABLE_MOCK_API !== 'false'),
+    import.meta.env.VITE_ENABLE_MOCK_API !== 'false',
 } as const
