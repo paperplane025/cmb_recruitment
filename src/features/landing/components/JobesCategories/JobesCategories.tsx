bdc8f81f863e9db3cb82d50b971f84ea4a286c82
NamLT <namlt@exidea.vn>
2026-07-12 15:13:22 +0700

    update code

--- src/features/landing/components/JobesCategories/JobesCategories.tsx
+++ src/features/landing/components/JobesCategories/JobesCategories.tsx
@@ -92,30 +92,6 @@ export function JobesCategories() {
               </div>
             </Link>
           ))}
-
-          {/* Render the 10th special text link box */}
-          <div className={styles['p-categories__list-box']} id="cat-card-list-box">
-            <h3 className={styles['p-categories__list-box-title']}>Danh sách danh mục</h3>
-            <Link
-              to={APP_ROUTES.jobs}
-              className={styles['p-categories__list-box-link']}
-              id="cat-view-all-link"
-            >
-              Xem tất cả danh mục
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2.5"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <line x1="5" y1="12" x2="19" y2="12" />
-                <polyline points="12 5 19 12 12 19" />
-              </svg>
-            </Link>
-          </div>
         </div>
         )}
       </div>
